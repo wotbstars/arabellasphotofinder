5f96923608b0504960cb2785e2400c5a7b4cbb96
stan <wotbstars@gmail.com>
2022-06-15 21:05:46 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("hello world");
+
+function add2(value) {
+  return value + 2;
+}
+
+function showlink() {
+  let ix = document.getElementById("search1").value;
+  window.open("https://www.bing.com/search?q=dog pictures "+ix, 'new');
+}
